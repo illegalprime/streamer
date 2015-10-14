5e6075e988e3a18aff89a011d9d17916e310fea2
Michael <themichaeleden@gmail.com>
2015-10-14 23:52:08 +0100

    node-server first pass

--- node-server/index.js
+++ node-server/index.js
@@ -3,14 +3,36 @@
 
 var ws = require("ws");
 var samples = require("./res/samples.js");
+var Camera = require("v4l2camera").Camera;
+var EventEmitter = require("events").EventEmitter;
+var Jpeg = require("jpeg-fresh").Jpeg;
+
+var emitter = new EventEmitter();
+
+var camera = new Camera("/dev/video1");
+camera.start();
+var interval = Math.round((1 / 30) * 1000);
+setTimeout(function reframe() {
+    camera.capture(function(success) {
+        if (success) {
+            var frame = new Buffer(camera.toRGB());
+            var jpeg = new Jpeg(frame.data, camera.width, camera.height, "rgb");
+            var encoded = jpeg.encodeSync();
+            emitter.emit("frame", encoded);
+        }
+        setTimeout(reframe, interval);
+    })
+}, interval); // 30 FPS
 
 var Server = new ws.Server({
     port: 9998,
 });
 
 Server.on("connection", function(client) {
-    client.send(samples.image.u8, {
-        binary: true,
-        mask: true,
+    emitter.on("frame", function(frame) {
+        client.send(frame, {
+            binary: true,
+            mask: true,
+        });
     });
 });
