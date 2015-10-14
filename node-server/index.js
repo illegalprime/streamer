2ed780f8cadbd0a55fb8bf33ad5bf3e9b6f42398
Michael <themichaeleden@gmail.com>
2015-10-14 21:02:51 +0100

    JPEG from server to client (node & browser)

--- node-server/index.js
+++ node-server/index.js
@@ -1,1 +1,16 @@
+#!/usr/bin/env node
+"use strict";
 
+var ws = require("ws");
+var samples = require("./res/samples.js");
+
+var Server = new ws.Server({
+    port: 9998,
+});
+
+Server.on("connection", function(client) {
+    client.send(samples.image.u8, {
+        binary: true,
+        mask: true,
+    });
+});
