edaf2cc341467cce0be4d60599b17f0d0099aa3c
Michael <themichaeleden@gmail.com>
2015-10-16 02:23:18 +0100

    Added framework for picking best configuration for any v4l camera

--- v4l2-quick/src/lib.rs
+++ v4l2-quick/src/lib.rs
@@ -1,82 +1,390 @@
 extern crate rscam;
 
 use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::fmt::Debug;
+use std::fmt::Formatter;
+use std::fmt::Result as FmtResult;
 
-pub use self::rscam::{Camera, Config, FormatInfo, ResolutionInfo, IntervalInfo};
+pub use self::rscam::{Camera, Config, FormatInfo, FormatIter, ResolutionInfo, IntervalInfo};
 pub use self::rscam::Result as V4l2Result;
+pub use self::rscam::consts;
 
-const MJPG: &'static [u8] = b"MJPG";
-const MIN_RES: (u32, u32) = (640, 480);
+pub enum DisStepInfo {
+    Discretes(Vec<(u32, u32)>),
+    Stepwise {
+        min: (u32, u32),
+        max: (u32, u32),
+        step: (u32, u32),
+    },
+}
 
-pub fn camera(path: &str) -> V4l2Result<(Camera, u32)> {
-    let mut camera = Camera::new(path).unwrap();
+#[derive(Clone)]
+pub enum Dir {
+    Highest,
+    Lowest,
+}
 
-    let mut formats: Vec<FormatInfo> = camera.formats()
-        .filter_map(Result::ok)
-        .collect();
-    formats.sort_by(|a, b| {
-        if a.emulated && !b.emulated {
-            return Ordering::Less;
-        } else if b.emulated && !a.emulated {
-            return Ordering::Greater;
+pub enum Pref {
+    Only,
+    Never,
+    Prefer,
+    DoNotPrefer,
+    NoPreference,
+}
+
+pub struct Fmt {
+    emulate: Pref,
+    compress: Pref,
+    priorities: Option<Vec<&'static [u8; 4]>>,
+}
+
+impl Default for Fmt {
+    fn default() -> Self {
+        Fmt {
+            emulate: Pref::NoPreference,
+            compress: Pref::NoPreference,
+            priorities: None,
         }
-        if a.compressed && !b.compressed {
-            return Ordering::Greater;
-        } else if b.compressed && !a.compressed {
-            return Ordering::Less;
-        }
-        if a.format == MJPG && b.format != MJPG {
-            return Ordering::Greater;
-        } else if b.format == MJPG && a.format != MJPG {
-            return Ordering::Less;
-        }
-        Ordering::Equal
-    });
-    let format = formats.last().unwrap();
-    let format = format.format;
+    }
+}
 
-    let resolution = match camera.resolutions(&format).unwrap() {
-        ResolutionInfo::Discretes(discretes) => {
-            let mut discretes: Vec<(u32, u32)> = discretes.into_iter()
-                .filter(|r| r.0 >= MIN_RES.0 && r.1 >= MIN_RES.1)
-                .collect();
-            discretes.sort();
-            discretes.first().unwrap().clone()
-        },
-        ResolutionInfo::Stepwise{ min, step, .. } => {
-            if min >= MIN_RES {
-                min
-            } else {
-                let steps_x = ((MIN_RES.0 - min.0 / step.0) as f64 + 0.5) as u32;
-                let steps_y = ((MIN_RES.1 - min.1 / step.1) as f64 + 0.5) as u32;
-                let steps = if steps_x > steps_y {
-                    steps_x
-                } else {
-                    steps_y
-                };
-                (min.0 + step.0 * steps, min.1 + step.1 * steps)
-            }
-        },
-    };
+#[derive(Clone)]
+struct DisStepConstraint {
+    pub dir: Dir,
+    pub limit: Option<(u32, u32)>,
+}
 
-    let interval = match camera.intervals(&format, resolution).unwrap() {
-        IntervalInfo::Discretes(mut discretes) => {
-            discretes.sort_by(|a, b| {
-                (a.0 as f32 / a.1 as f32).partial_cmp(&(b.0 as f32 / b.1 as f32)).unwrap()
+pub type Res = DisStepConstraint;
+pub type Speed = DisStepConstraint;
+
+pub struct Constraints {
+    pub formats: Option<Fmt>,
+    pub resolutions: Option<Res>,
+    pub speeds: Option<Speed>,
+    pub field: u32,
+    pub nbuffers: u32,
+}
+
+impl Default for Constraints {
+    fn default() -> Self {
+        Constraints {
+            formats: None,
+            resolutions: None,
+            speeds: None,
+            field: consts::FIELD_NONE,
+            nbuffers: 2,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct ConfigSummary {
+    pub interval: (u32, u32),
+    pub resolution: (u32, u32),
+    pub format: [u8; 4],
+    pub field: u32,
+    pub nbuffers: u32,
+}
+
+impl Debug for ConfigSummary {
+    fn fmt(&self, fmt: &mut Formatter) -> FmtResult {
+        try!(fmt.write_str("{\n"));
+        try!(fmt.write_fmt(format_args!(
+            "    interval: {}/{},\n", self.interval.0, self.interval.1)));
+        try!(fmt.write_fmt(format_args!(
+            "    resolution: {}x{},\n", self.resolution.0, self.resolution.1)));
+        try!(fmt.write_fmt(format_args!(
+            "    picture_format: {},\n", String::from_utf8_lossy(&self.format))));
+        try!(fmt.write_fmt(format_args!(
+            "    field: {}\n", self.field)));
+        try!(fmt.write_fmt(format_args!(
+            "    nbuffers: {}\n", self.nbuffers)));
+        fmt.write_str("}\n")
+    }
+}
+
+struct FormatPicker {
+    formats: Vec<FormatInfo>,
+    constraints: Option<Fmt>,
+    sorted: bool,
+}
+
+impl FormatPicker {
+    pub fn new(formats: FormatIter, constraints: Option<Fmt>) -> Self {
+        FormatPicker {
+            constraints: constraints,
+            formats: formats.filter_map(Result::ok).collect(),
+            sorted: false,
+        }
+    }
+
+    fn sort(constraints: &Fmt, formats: &mut Vec<FormatInfo>) {
+        // This has to only be done once, it can be slow
+        // for the sake of clarity
+        // Filter if user cares about emulated formats
+        let filter_emulate = match constraints.emulate {
+            Pref::Only => Some(true),
+            Pref::Never => Some(false),
+            _ => None,
+        };
+        if let Some(emulate) = filter_emulate {
+            formats.retain(|f| {
+                f.emulated && emulate || !f.emulated && !emulate
             });
-            discretes.first().unwrap().clone()
-        },
-        IntervalInfo::Stepwise{ min, .. } => min,
-    };
+        }
+        // Filter is user cares about compression
+        let filter_compress = match constraints.compress {
+            Pref::Only => Some(true),
+            Pref::Never => Some(false),
+            _ => None,
+        };
+        if let Some(compress) = filter_compress {
+            formats.retain(|f| {
+                f.compressed && compress || !f.compressed && !compress
+            });
+        }
+        // Create a map of formats to their priorities
+        let wanted: Option<HashMap<&[u8; 4], usize>> = constraints.priorities.as_ref().map(|vec| {
+            vec.into_iter()
+                .enumerate()
+                .fold(HashMap::new(), |mut map, (index, format)| {
+                    map.insert(*format, index);
+                    map
+                })
+        });
+        // Remove all formats not in the priorities list
+        if let Some(ref priorities) = wanted {
+            formats.retain(|f| {
+                priorities.get(&f.format).is_some()
+            });
+        }
+        // Sort formats based on preferences and priorities
+        formats.sort_by(|a, b| {
+            // Sort first by priority
+            if let Some(ref priorities) = wanted {
+                let a_priority = priorities.get(&a.format);
+                let b_priority = priorities.get(&b.format);
+                if let (Some(ap), Some(bp)) = (a_priority, b_priority) {
+                    if ap < bp {
+                        return Ordering::Greater;
+                    } else if ap > bp {
+                        return Ordering::Less;
+                    }
+                }
+            }
+            // Next sort by emulation preferences
+            let decision = match (a.emulated, b.emulated, &constraints.emulate) {
+                (true, false, &Pref::Prefer) => Some(Ordering::Greater),
+                (false, true, &Pref::Prefer) => Some(Ordering::Less),
+                (true, false, &Pref::DoNotPrefer) => Some(Ordering::Less),
+                (false, true, &Pref::DoNotPrefer) => Some(Ordering::Greater),
+                _ => None,
+            };
+            if let Some(concrete) = decision {
+                return concrete;
+            }
+            // Finally sort by compression preferences
+            let decision = match (a.compressed, b.compressed, &constraints.compress) {
+                (true, false, &Pref::Prefer) => Some(Ordering::Greater),
+                (false, true, &Pref::Prefer) => Some(Ordering::Less),
+                (true, false, &Pref::DoNotPrefer) => Some(Ordering::Less),
+                (false, true, &Pref::DoNotPrefer) => Some(Ordering::Greater),
+                _ => None,
+            };
+            if let Some(concrete) = decision {
+                return concrete;
+            }
+            // We cannot decide! They are equal
+            Ordering::Equal
+        });
+    }
+}
 
-    let config = Config {
-        interval: interval,
-        resolution: resolution,
-        format: &format,
+impl Iterator for FormatPicker {
+    type Item = FormatInfo;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(ref constraints) = self.constraints {
+            if !self.sorted {
+                FormatPicker::sort(constraints, &mut self.formats);
+            }
+        }
+        self.formats.pop()
+    }
+}
+
+impl Into<DisStepInfo> for ResolutionInfo {
+    fn into(self) -> DisStepInfo {
+        match self {
+            ResolutionInfo::Discretes(d) => DisStepInfo::Discretes(d),
+            ResolutionInfo::Stepwise{min, max, step} => {
+                DisStepInfo::Stepwise {
+                    min: min,
+                    max: max,
+                    step: step
+                }
+            },
+        }
+    }
+}
+
+impl Into<DisStepInfo> for IntervalInfo {
+    fn into(self) -> DisStepInfo {
+        match self {
+            IntervalInfo::Discretes(d) => DisStepInfo::Discretes(d),
+            IntervalInfo::Stepwise{min, max, step} => {
+                DisStepInfo::Stepwise {
+                    min: min,
+                    max: max,
+                    step: step
+                }
+            },
+        }
+    }
+}
+
+pub struct DisStepPicker {
+    info: DisStepInfo,
+    constraints: Option<DisStepConstraint>,
+    sorted: bool,
+}
+
+impl DisStepPicker {
+    fn new<I>(info: I, constraints: Option<DisStepConstraint>) -> Self
+    where I: Into<DisStepInfo> {
+        DisStepPicker {
+            info: info.into(),
+            constraints: constraints,
+            sorted: false,
+        }
+    }
+
+    fn sort_discretes(discretes: &mut Vec<(u32, u32)>, constraints: &DisStepConstraint) {
+        let want_highest = match constraints.dir {
+            Dir::Highest => true,
+            Dir::Lowest => false,
+        };
+        if let Some(ref limit) = constraints.limit {
+            discretes.retain(|r| {
+                (want_highest && r <= limit) || (!want_highest && r >= limit)
+            });
+        }
+        if want_highest {
+            discretes.sort();
+        } else {
+            // Sort in reverse
+            discretes.sort_by(|a, b| b.cmp(a));
+        }
+    }
+}
+
+impl Iterator for DisStepPicker {
+    type Item = (u32, u32);
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.info {
+            DisStepInfo::Discretes(ref mut discretes) => {
+                if let Some(ref constraints) = self.constraints {
+                    if !self.sorted {
+                        DisStepPicker::sort_discretes(discretes, constraints);
+                    }
+                }
+                discretes.pop()
+            },
+            DisStepInfo::Stepwise{ .. } => {
+                unimplemented!();
+            },
+        }
+    }
+}
+
+fn get_config(camera: &Camera, reqs: Constraints) -> Option<ConfigSummary> {
+    let formats = FormatPicker::new(camera.formats(), reqs.formats);
+    for format in formats {
+        if let Ok(resolutions) = camera.resolutions(&format.format) {
+            let resolutions = DisStepPicker::new(resolutions, reqs.resolutions.clone());
+            for resolution in resolutions {
+                if let Ok(intervals) = camera.intervals(&format.format, resolution) {
+                    let intervals = DisStepPicker::new(intervals, reqs.speeds.clone());
+                    for interval in intervals {
+                        return Some(ConfigSummary {
+                            interval: interval,
+                            resolution: resolution,
+                            format: format.format,
+                            field: reqs.field,
+                            nbuffers: reqs.nbuffers,
+                        });
+                    }
+                }
+            }
+        }
+    }
+    None
+}
+
+pub fn camera(path: &str, reqs: Constraints) -> V4l2Result<(Camera, Option<ConfigSummary>)> {
+    let mut camera = try!(Camera::new(path));
+    let possible_config = get_config(&camera, reqs);
+    if let Some(config) = possible_config.clone() {
+        try!(camera.start(&Config {
+            interval: config.interval,
+            resolution: config.resolution,
+            format: &config.format[..],
+            field: config.field,
+            nbuffers: config.nbuffers,
+        }));
+    }
+    Ok((camera, possible_config))
+}
+
+#[test]
+fn optomize_framerate() {
+    let constraints = Constraints {
+        formats: Some(Fmt {
+            emulate: Pref::DoNotPrefer,
+            compress: Pref::Prefer,
+            priorities: Some(vec![b"MJPG"]),
+        }),
+        resolutions: Some(Res {
+            dir: Dir::Lowest,
+            limit: Some((640, 480)),
+        }),
+        speeds: Some(Speed {
+            dir: Dir::Highest,
+            limit: None,
+        }),
         .. Default::default()
     };
 
-    camera.start(&config).unwrap();
-    let refresh = ((interval.0 as f32 / interval.1 as f32) * 1000. + 0.5) as u32;
-    Ok((camera, refresh))
+    if let Ok((_, possible_config)) = camera("/dev/video1", constraints) {
+        println!("Camera started successfuly");
+        let config = possible_config.expect("Could not find a configuration!");
+        println!("Camera Configuration for Framerate: {:?}", &config);
+    } else {
+        panic!("Could not get camera!");
+    }
+}
+
+#[test]
+fn optomize_quality() {
+    let constraints = Constraints {
+        formats: Some(Fmt {
+            emulate: Pref::NoPreference,
+            compress: Pref::DoNotPrefer,
+            priorities: Some(vec![b"MJPG"]),
+        }),
+        resolutions: Some(Res {
+            dir: Dir::Highest,
+            limit: None,
+        }),
+        speeds: None,
+        .. Default::default()
+    };
+
+    if let Ok((_, possible_config)) = camera("/dev/video1", constraints) {
+        println!("Camera started successfuly");
+        let config = possible_config.expect("Could not find a configuration!");
+        println!("Camera Configuration for Quality: {:?}", &config);
+    } else {
+        panic!("Could not get camera!");
+    }
 }
