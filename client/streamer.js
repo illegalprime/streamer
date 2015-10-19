b3f0f3519cbddfb5efa91b067f437cc1bbc2703b
Michael Eden <themichaeleden@gmail.com>
2015-10-18 23:49:03 -0400

    Correctly handle frames larger than 65536 bytes

--- client/streamer.js
+++ client/streamer.js
@@ -6,6 +6,9 @@ var Streamers;
         server: "ws://127.0.0.1:9998/",
         canvas: ".streamer-video",
     };
+    var FULL_IMAGE_PREFIX = 0x55;
+    var CAMERA_IN_USE = 0x33;
+    var MAX_PACKET_SIZE = 65536;
 
     Streamers = function(options) {
         var opts = _.extend(defaults, options);
@@ -40,21 +43,35 @@ var Streamers;
             image.src = img_url;
         };
 
+        var buffer = [];
         conn.onmessage = function(event) {
-            if (event.data.size > 1) {
-                if (!paused) {
-                    _.each(contexts, jpgToCanvas.bind(undefined, event.data));
-                }
-            } else {
+            if (event.data.size === 1) {
                 // Received special message from server
+            } else {
+                buffer.push(event.data);
+                if (event.data.size < MAX_PACKET_SIZE) {
+                    var blob;
+                    if (buffer.length > 0) {
+                        blob = new Blob(buffer, {
+                            type: "image/jpeg"
+                        });
+                        buffer = [];
+                    } else {
+                        blob = event.data;
+                    }
+                    console.log(blob.size);
+                    if (!paused) {
+                        _.each(contexts, jpgToCanvas.bind(undefined, blob));
+                    }
+                }
             }
         };
 
         self.photograph = function(done) {
             want_picture.push(done);
-            if (taking_picture) {
-                return;
-            }
+            // if (taking_picture) {
+            //     return;
+            // }
             conn.send("capture");
             taking_picture = true;
         };
